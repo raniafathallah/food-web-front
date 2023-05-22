abe535b824bfedd03d99dae4e6085f2fb5d12b20
raniafathallah <raniaahmed7000@gmail.com>
2023-05-22 04:00:49 +0300

    build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
      production: false,
-    BASE_URL : 'http://localhost:7000',
-    PRODUCT_URL: 'http://localhost:7000'+'/api/products',
-    USER_URL: 'http://localhost:7000'+'/api/users',
+
+    PRODUCT_URL: 'https://food-api-vfo7.onrender.com'+'/api/products',
+    USER_URL: 'https://food-api-vfo7.onrender.com'+'/api/users',
 
    };
 
